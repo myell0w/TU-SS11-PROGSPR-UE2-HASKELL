9bc383d10e0c861e53b77752955eacf8dd3bfeaa
Matthias Tretter <matthias.tretter@gmail.com>
2011-04-25 16:21:32 +0200

    Added empty file SeatReservation.hs

---
+++ SeatReservation.hs
@@ -0,0 +1,11 @@
+-- Programmiersprachen, VL
+-- SS 2011
+-----------------------------------------
+-- Matthias Tretter, 0726390
+-- Philip Messlehner
+-- Thomas Perl
+-----------------------------------------
+-- SeatReservation.hs
+-----------------------------------------
+
+
